b5827afb8c0010047d0dd67217159cd1aa467ea2
Unix One <unix1@live.com>
2021-06-19 09:16:11 -0700

    Add simple uuid command

---
+++ src/rinseweb_wiz_uuid.erl
@@ -0,0 +1,21 @@
+%%%-------------------------------------------------------------------
+%% @doc rinseweb UUID wizard
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(rinseweb_wiz_uuid).
+
+%% API
+-export([answer/2]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+-spec answer(rinseweb_wiz:question(), [any()]) -> rinseweb_wiz:answer().
+answer(<<"uuid">>, []) ->
+    #{
+        question => <<"uuid">>,
+        type => text,
+        short => list_to_binary(uuid:uuid_to_string(uuid:get_v4()))
+    }.
