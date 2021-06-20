776892dd2613c109b4985a0ec2bc1341f5bdaedf
Unix One <unix1@live.com>
2021-06-20 09:35:59 -0700

    Link to about and commands pages, minor UI fixes

--- src/rinseweb_h_root.erl
+++ src/rinseweb_h_root.erl
@@ -37,28 +37,33 @@ hello_to_html(Req, State) ->
 	<title>rinse</title>
     <script type=\"text/javascript\" src=\"/assets/app.js\" nonce=\"",Nonce/binary,"\"></script>
     <style type=\"text/css\">
-        h2 {font-size:5em;font-family: monospace;}
+        h2 {font-size:5em;font-family:monospace;}
         input[type=\"text\"] {font-size:3em;}
-        #answer {font-size:2em;}
+        #answer {font-size:3em;font-family:sans-serif;}
+        footer {position:fixed;bottom:0;text-align:center;width:100%;font-size:1em;font-family:sans-serif;}
     </style>
 </head>
 <body>
     <center>
         <h2><span style=\"color:#0f2557\">r</span><span style=\"color:#28559a\">i</span><span style=\"color:#3778c2\">n</span><span style=\"color:#4b9fe1\">s</span><span style=\"color:#63bce5\">e</span></h2>
         <form id=\"search\">
-            <input id=\"question\" type=\"text\" size=\"30\" style=\"text-align:center\" />
+            <input id=\"question\" type=\"text\" autocapitalize=\"none\" size=\"30\" style=\"text-align:center\" />
         </form>
-        <br/>
+        <br/><br/>
         <div id=\"answer\">
         </div>
     </center>
+<footer>
+    <p><a href=\"/about\">About</a> - <a href=\"/commands\">Commands</a></p>
+</footer>
 </body>
 </html>">>,
     {Body, Req, State}.
 
 hello_to_json(Req, State) ->
-    Body = <<"{\"rest\": \"Hello World!\"}">>,
+    Body = <<"{\"rinse\": \"rinse is not a search engine.\"}">>,
     {Body, Req, State}.
 
 hello_to_text(Req, State) ->
-    {<<"REST Hello World as text!">>, Req, State}.
+    Body = <<"rinse\nrinse is not a search engine.">>,
+    {Body, Req, State}.
