085b6a7e34e3569c458fb8f8723a23c71d71348d
Unix One <unix1@live.com>
2021-08-04 23:40:36 -0700

    Add dictionaryapi implementation and tests

--- priv/static/assets/app.js
+++ priv/static/assets/app.js
@@ -35,32 +35,83 @@ function answerToNode(answer) {
     var node;
     switch(answer.type) {
         case "text":
-            node = document.createTextNode(answer.answer.text);
+            node = document.createElement('div');
+            node.setAttribute('class', 'simple');
+            node.appendChild(document.createTextNode(answer.answer.text));
             break;
         case "hash":
-            node = document.createTextNode(answer.answer.hash);
+            node = document.createElement('div');
+            node.setAttribute('class', 'simple');
+            node.appendChild(document.createTextNode(answer.answer.hash));
             break;
         case "conversion_result":
             const a = answer.answer;
             var txt = a.unit_from_number + " " + a.unit_from_name + " = " + a.unit_to_number + " " + a.unit_to_name
-            node = document.createTextNode(txt);
+            node = document.createElement('div');
+            node.setAttribute('class', 'simple');
+            node.appendChild(document.createTextNode(txt));
             break;
         case "wiki":
             node = document.createElement('ul');
-            results = answer.answer;
-            for (let i = 0; i < results.length; i++) {
+            node.setAttribute('class', 'wiki');
+            answer.answer.forEach(function(result) {
                 var itemNode = document.createElement('li');
                 var linkNode = document.createElement('a');
-                var linkAttr = document.createAttribute('href');
-                linkAttr.value = results[i].url;
-                linkNode.setAttributeNode(linkAttr);
-                linkNode.appendChild(document.createTextNode(results[i].title));
+                linkNode.setAttribute('href', result.url);
+                linkNode.appendChild(document.createTextNode(result.title));
                 var descNode = document.createElement('span');
-                descNode.innerHTML = results[i].snippet;
+                descNode.innerHTML = result.snippet;
                 itemNode.appendChild(linkNode);
                 itemNode.appendChild(descNode);
                 node.appendChild(itemNode);
-            }
+            });
+            break;
+        case "definition":
+            node = document.createElement('ul');
+            node.setAttribute('class', 'definition');
+            answer.answer.forEach(function(result) {
+                var wordNode = document.createElement('li');
+                var wordHeader = document.createElement('header');
+                var wordText = document.createElement('span');
+                wordText.setAttribute('class', 'word');
+                wordText.textContent = result.word;
+                // phonetics
+                var phoneticsList = document.createElement('ul');
+                phoneticsList.setAttribute('class', 'phonetics');
+                result.phonetics.forEach(function(p) {
+                    var phoneticsItem = document.createElement('li');
+                    var phoneticsItemLink = document.createElement('a');
+                    phoneticsItemLink.setAttribute('href', p.audio);
+                    phoneticsItemLink.appendChild(document.createTextNode(p.text));
+                    phoneticsItem.appendChild(phoneticsItemLink);
+                    phoneticsList.appendChild(phoneticsItem);
+                });
+                wordHeader.appendChild(wordText);
+                wordHeader.appendChild(phoneticsList);
+                wordNode.appendChild(wordHeader);
+                // meanings
+                var meaningsList = document.createElement('ul');
+                meaningsList.setAttribute('class', 'meanings');
+                result.meanings.forEach(function (m) {
+                    var meaningsItem = document.createElement('li');
+                    meaningsItem.appendChild(document.createTextNode(m.part_of_speech));
+                    var defsList = document.createElement('ul');
+                    m.definitions.forEach(function(d) {
+                        var defsItem = document.createElement('li');
+                        defsItem.appendChild(document.createTextNode(d.definition));
+                        if (d.example !== undefined) {
+                            var defsExample = document.createElement('div');
+                            defsExample.appendChild(document.createTextNode("example: " + d.example));
+                            defsItem.appendChild(defsExample);
+                        }
+                        defsList.appendChild(defsItem);
+                    });
+                    meaningsItem.appendChild(defsList);
+                    meaningsList.appendChild(meaningsItem);
+                });
+                wordNode.appendChild(meaningsList);
+                node.appendChild(wordNode);
+            });
             break;
         default:
             node = document.createTextNode("");
