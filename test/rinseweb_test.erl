45715df61a568ea8e944f53e8d4b72598f88f76c
Unix One <unix1@live.com>
2023-07-26 00:33:06 -0700

    Unexpose now internal functions in rinseweb_test helper

--- test/rinseweb_test.erl
+++ test/rinseweb_test.erl
@@ -2,12 +2,8 @@
 
 %% API
 -export([base_uri/0]).
--export([question_uri/1]).
 -export([request_and_decode_answer/1]).
 -export([request_and_decode_answer/2]).
--export([request_json/1]).
--export([request_json/2]).
--export([decode_response_body/1]).
 
 %%====================================================================
 %% API
@@ -17,16 +13,13 @@ base_uri() ->
     Port = integer_to_list(rinseweb_env:port()),
     "http://localhost:" ++ Port.
 
-question_uri(Question) ->
-     base_uri() ++ "/answer/" ++ edoc_lib:escape_uri(Question).
-
 request_and_decode_answer(Question) ->
     request_and_decode_answer(Question, []).
 
 request_and_decode_answer(Question, ExtraHeaders) ->
     {ok, {{"HTTP/1.1", 200, "OK"}, Headers, ResponseBody}} = request_json(Question, ExtraHeaders),
     true = lists:member({"content-type","application/json"}, Headers),
-    ResponseMap = rinseweb_test:decode_response_body(ResponseBody),
+    ResponseMap = decode_response_body(ResponseBody),
     ExpectedQuestion = rinseweb_util:binary_trim(list_to_binary(Question)),
     ExpectedQuestion = maps:get(<<"question">>, ResponseMap),
     [Answer|_] = maps:get(<<"answers">>, ResponseMap),
@@ -34,8 +27,12 @@ request_and_decode_answer(Question, ExtraHeaders) ->
     true = maps:is_key(<<"type">>, Answer),
     Answer.
 
-request_json(Question) ->
-    request_json(Question, []).
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+question_uri(Question) ->
+     base_uri() ++ "/answer/" ++ edoc_lib:escape_uri(Question).
 
 request_json(Question, ExtraHeaders) ->
     Headers = [{"Accept", "application/json"}|ExtraHeaders],
